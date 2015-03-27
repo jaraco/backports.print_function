3eefc1572c5956911e9347960041659634b4d50d
Jason R. Coombs <jaraco@jaraco.com>
2015-03-27 09:08:14 -0400

    Remove sphinx docs

--- setup.py
+++ setup.py
@@ -12,15 +12,13 @@ with io.open('README.txt', encoding='utf-8') as readme:
 
 needs_pytest = {'pytest', 'test'}.intersection(sys.argv)
 pytest_runner = ['pytest_runner'] if needs_pytest else []
-needs_sphinx = {'release', 'build_sphinx', 'upload_docs'}.intersection(sys.argv)
-sphinx = ['sphinx'] if needs_sphinx else []
 
 setup_params = dict(
 	name='backports.print_function',
 	use_scm_version=True,
 	author="Jason R. Coombs",
 	author_email="jaraco@jaraco.com",
-	description="backports.print_function",
+	description="Print function with Python 3.3 flush argument",
 	long_description=long_description,
 	url="https://bitbucket.org/jaraco/backports.print_function",
 	packages=setuptools.find_packages(),
@@ -29,7 +27,7 @@ setup_params = dict(
 	],
 	setup_requires=[
 		'setuptools_scm',
-	] + pytest_runner + sphinx,
+	] + pytest_runner,
 	tests_require=[
 		'pytest',
 	],
